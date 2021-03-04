c35331491b1ff8511207c41486637790c75a2ed3
eliane <elianekelm21@gmail.com>
2021-03-04 19:33:11 -0300

    Initial commit

---
+++ cap02/exercicios.py
@@ -0,0 +1,17 @@
+print("eliane") # Exibir o nome na tela
+
+a = 3  # Exibir resultado de 2a * 3b
+b = 5
+resul = (2*a) * (3*b)
+print(resul)
+
+a = 10   #Calcular a soma de 3 variáveis e imprimir o resultado
+b = 2.5
+c = 99
+print(a+b+c)
+
+salario = 750     #Calcular aumento salarial de 15% sobre R$750
+aum_perc = 0.15
+novo_salario = salario*aum_perc
+print(f"Aumento  R$ : {novo_salario}")
+
