4f069392d08652d3fd573b1228b556b2ae688a1f
eliane <elianekelm21@gmail.com>
2021-03-05 18:07:20 -0300

    exercise 3.7

---
+++ cap03-variaveis/exer3_7.py
@@ -0,0 +1,8 @@
+#Faça um programa que peça dois números inteiros. Imprima a soma deles.
+
+print("*** Soma de dois números inteiros ***".upper())
+num1 = int(input("Digite o 1º número: "))
+num2 = int(input("Digite o 2º número: "))
+
+soma = num1 + num2
+print(f"{num1} + {num2} = {soma}")
