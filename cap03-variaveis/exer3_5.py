ff2c1777121e96ee63dc0a35ac90456b200e2849
eliane <elianekelm21@gmail.com>
2021-03-05 18:00:03 -0300

    exercice 3.4

---
+++ cap03-variaveis/exer3_5.py
@@ -0,0 +1,9 @@
+mat1 = float(input("Digite a nota da 1ª matéria: "))
+mat2 = float(input("Digite a nota da 2ª matéria: "))
+mat3 = float(input("Digite a nota da 3ª matéria: "))
+
+
+if (mat1 >= 7 and mat2 >= 7 and mat3 >=7):
+    print(f"Parabéns você foi aprovado!")
+else:
+    print("Infelizmente você não apravado. Continue estudando!")
