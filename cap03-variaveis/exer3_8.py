fbac44c6442800a04a5215a2c282e79e740d5cbe
eliane <elianekelm21@gmail.com>
2021-03-05 18:19:35 -0300

    transforma metros em milímetros

---
+++ cap03-variaveis/exer3_8.py
@@ -0,0 +1,6 @@
+#Escreva um programa que leia um valor em metros e o exiba convertido em milímetros
+vl_metros = float(input("Informe o valor em metros (m): "))
+
+vl_mil = vl_metros * 1000
+
+print(f"{vl_metros:5.2f} metros equivale a {vl_mil:5.2f} milímetros.")
