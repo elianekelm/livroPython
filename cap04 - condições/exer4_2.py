418ebfe74ccdccc8c7f4e8aaafd08794ab215a7b
eliane <elianekelm21@gmail.com>
2021-03-07 12:42:28 -0300

    verificação de velocidade - multa acima de 80km/h

--- cap04 - condições/exer4_2.py
+++ cap04 - condições/exer4_2.py
@@ -1,7 +1,7 @@
 print("\n***** VERIFICAÇÃO DE VELOCIDADE *****")
 speed = float(input("Digite a velocidade do carro (Km/h): "))
 
-if speed >= 80:
+if speed > 80:
     multa = (speed - 80) * 5
     print(f"Multa R$ {multa:3.2f} - (Ultrapassar a velocidade permitida)")
 else:
